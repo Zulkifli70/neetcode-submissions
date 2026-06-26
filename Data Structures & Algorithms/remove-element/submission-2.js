704789f95716fc4ecc0859fcf7304a3090338bb7
Zulkifli Firdausi <65410126+Zulkifli70@users.noreply.github.com>
2026-06-26 16:51:44 +0700

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums, val) {
+        let k = 0
+        for(const num of nums){
+            if(num !== val){
+                nums[k] = num;
+                k++
+            }
+        }
+        return k
+    }
+}
