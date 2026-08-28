b00d8eb56dc0f0531ba69f83e2e1935037f33755
Zulkifli Firdausi <65410126+Zulkifli70@users.noreply.github.com>
2026-08-28 13:24:19 +0700

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let stack = []
+        let pairs = { ')': '(', ']': '[', '}': '{' }
+
+        for(let i = 0; i < s.length; i++){
+            let char = s[i]
+
+            if(char === '(' || char === '[' || char === '{'){
+                stack.push(char)
+            } else {
+                 if (stack.length === 0 || stack.pop() !== pairs[char]) {
+                return false
+                 }
+            }
+        }
+        return stack.length === 0
+    }
+}
