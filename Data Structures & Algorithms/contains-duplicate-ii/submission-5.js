96972431534c014fb9bb95ef7e8ff8d6390fc3e6
Zulkifli Firdausi <65410126+Zulkifli70@users.noreply.github.com>
2026-09-21 17:02:18 +0700

    Add: contains-duplicate-ii - submission-5

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-5.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {boolean}
+     */
+    containsNearbyDuplicate(nums, k) {
+        const seen = new Set()
+
+        for(let i = 0; i < nums.length; i++){
+            if(seen.has(nums[i])){
+                return true
+            }
+
+            seen.add(nums[i])
+
+            if(seen.size > k){
+                seen.delete(nums[i - k])
+            }
+        }
+        return false
+    }
+}
